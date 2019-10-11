b0a2877d13a98c701ae7be81c14a6faec31d78f9
wickyyang <wicky1104@gmail.com>
2019-10-11 10:27:08 +0800

    update EVM interpreter version and add create2 test

--- contracts/compile_contracts.sh
+++ contracts/compile_contracts.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-docker run --rm -v `pwd`:/contracts ethereum/solc:0.4.25 \
+docker run --rm -v `pwd`:/contracts ethereum/solc:0.5.0 \
+    --evm-version constantinople \
     --bin --abi -o /contracts/abi_bin \
-    --overwrite /contracts/token.sol /contracts/kv_storage.sol /contracts/vari_and_fun.sol /contracts/limit.sol
-
-docker run --rm -v `pwd`:/contracts ethereum/solc:0.5.1 \
-    --bin --abi -o /contracts/abi_bin \
-    --overwrite /contracts/admin_op.sol
+    --overwrite /contracts/admin_op.sol /contracts/token.sol /contracts/kv_storage.sol /contracts/vari_and_fun.sol
 
 go generate gen.go
-
